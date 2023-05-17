e8bf07b51c944a548988cf3dd10c51a6f95af98d
zinovev.evgenii@mail.ru <zinovev.evgenii@mail.ru>
2023-05-17 15:40:08 +0300

    3

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from ice_cream.models import IceCream
 
 def index(request):
     template = 'homepage/index.html'
-    return render(request, template)
+    ice_cream_list = IceCream.objects.all()
+    context = {
+        'ice_cream_list': ice_cream_list,
+    }
+    return render(request, template, context)
